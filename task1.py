14bffb9a1651bf52aae08b0d5118b8cfb698ea48
sukharevskyi <Illia.Sukharevskyi@arrk-engineering.com>
2020-07-19 21:29:45 +0200

    Correct color ranges

--- task1.py
+++ task1.py
@@ -28,13 +28,17 @@ class ExtractGeometry:
 
         while ret and key != ord(' '):
             hsv_frm = cv2.cvtColor(frm, cv2.COLOR_RGB2HSV)
+            hsv_frm = cv2.GaussianBlur(hsv_frm, (5, 5), 3)
+
             submasks = self.collect_submasks(hsv_frm)
             full_mask = self.combine_masks(submasks)
 
             self.writer.write(full_mask)
+            cv2.imshow('Video frame', full_mask)
 
             ret, frm = self.cap.read()
             key = cv2.waitKey(self.video_delay)
+
 
         self.clean_up()
 
@@ -61,11 +65,22 @@ class ExtractGeometry:
 
 
 if __name__ == '__main__':
-    colors = [ColorRange("green", np.array([25, 52, 72]), np.array([102, 255, 255])),
+    colors = [ColorRange("green", np.array([20, 40, 100]), np.array([80, 100, 255])),
               ColorRange("blue", np.array([94, 80, 2]), np.array([126, 255, 255])),
               ColorRange("black", np.array([0, 0, 0]), np.array([180, 255, 50])),
               ColorRange("red", np.array([170, 120, 70]), np.array([180, 255, 255]))]
 
+    """[ColorRange("green", np.array([25, 52, 72]), np.array([102, 255, 255])),
+     ColorRange("blue", np.array([94, 80, 2]), np.array([126, 255, 255])),
+     ColorRange("black", np.array([0, 0, 0]), np.array([180, 255, 50])),
+     ColorRange("red", np.array([170, 120, 70]), np.array([180, 255, 255]))]
+    mask_gy = cv2.inRange(frm_hsv, np.array([20, 40, 100]), np.array([80, 100, 255]))
+
+    # mask for pink
+    mask_p = cv2.inRange(frm_hsv, np.array([140, 50, 0]), np.array([179, 150, 255]))
+
+    # mask for black
+    mask_b = cv2.inRange(frm_hsv, np.array([0, 0, 0]), np.array([179, 255, 40]))"""
     extractor = ExtractGeometry("input_video.avi", "output_video.avi",
                                 video_writer_type=cv2.VideoWriter_fourcc(*"XVID"),
                                 colors_to_extract=colors, video_delay=10)
